5140e24986686dbc8ba3146c9962b9dce56a5179
Doyal-N <coin20@protonmail.com>
2021-03-20 13:04:26 +0500

    add list#delete with stimulus

--- app/reflexes/list_reflex.rb
+++ app/reflexes/list_reflex.rb
@@ -2,4 +2,9 @@ class ListReflex < ApplicationReflex
   def create(title)
     List.create(title: title)
   end
+
+  def delete
+    list = List.find(element.dataset[:id])
+    list.delete
+  end
 end
